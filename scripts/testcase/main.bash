de24f302d5aa20d9af5d333bc14febb7d9e1fc59
Pirkka Ollinaho <pirkka.ollinaho@fmi.fi>
2017-03-28 21:44:05 +0300

    Unstable. testcase only works (on purpose), tried to clear up makefile rules and targets

--- scripts/testcase/main.bash
+++ scripts/testcase/main.bash
@@ -23,99 +23,123 @@ for i in $(seq 0 $ENS); do
     # Add 1-2 leading zeros if ens member < 100
     if [ $i -lt 10 ]; then i=00$i; elif [ $i -lt 100 ]; then i=0$i; fi
     mkdir -p  $DATA/$SDATE/pert$i
-    echo >    $DATA/$SDATE/pert$i/input
+    echo >    $DATA/$SDATE/pert$i/infile_new
 done
 
+
 # Print the makefile in explicit form
+#
 makemake () {
-    cdate=$1
-    njobs=$2
-    printf ".PHONY: all\n\n" 
-    printf "all: %s\n\n" "$NEW_INPUTS"
-    printf "%s : %s\n"   "$NEW_INPUTS" "$OUTPUTS2"
-    for item in $NEW_INPUTS; do makedirs="$makedirs $(dirname $item)"; done
-    printf "\tmkdir -p %s\n" "$makedirs"
-    printf "\t%s %s > %s\n\n" "$GENPARS" "$OUTPUTS2" "$NEW_INPUTS"
     
-    n=0
-    fflist=( $flist )
-    for item in $OUTPUTS2; do
-	printf "%s : %s\n" "$item" "${fflist[$n]}"
-	printf "\tcd %s ; echo > pp\n" "$(dirname $item)"
-	(( n+=1 ))
-    done
+    printf ".PHONY: all\n\n"
+    eval temp=$TARGET_A0 # cdate wont evaluate otherwise...
+    printf "all: %s\n\n"  "${temp}"
+    printf "%s : %s\n"    "${temp}"   "${ALL_TARGETS_A}"
+    printf "\t%s \n\n"    "${RULE_A}" 
     
-    n=0
-    fflist=( $flist )
-    for item in $OUTPUTS; do
-	printf "%s : %s\n" "$item" "${fflist[$n]}"
-	printf "\tcd %s ; %s\n" "$(dirname $item)" "$RUNEPS"
-	(( n+=1 ))
+    for itarget in $(echo {B..E}); do
+	eval ctarget='${'TARGET_${itarget}'0}'
+	eval dtarget='${'TARGET_${itarget}'1}'
+	eval etarget='${'RULE_${itarget}'}'
+	
+	if [ $itarget == "C" ]; then
+	    ddate=$ndate
+	else
+	    ddate=$cdate
+	fi
+	
+	printf "# Case %s = %s : %s\n" "$itarget" "$ctarget" "$dtarget"
+	
+	for i in $(seq 0 $ENS); do
+	    if [ $i -lt 10 ]; then i=00$i; elif [ $i -lt 100 ]; then i=0$i; fi
+	    if [ ! -z $dtarget ]; then
+		printf "%s/pert%s/%s : %s/pert%s/%s\n" "$ddate" $i \
+		       "${ctarget##*/}" "$cdate" $i "${dtarget##*/}"
+	    else
+		printf "%s/pert%s/%s : \n" "$cdate" $i "${ctarget##*/}"
+	    fi
+	done
+	printf "\t%s\n\n" "$etarget"
     done
 }
 
-# Variables for make
+# Targets and rules for makefile
 #
-# GENERATE   - command to generate NEW_INPUTS from OUTPUTS
-# RUNEPS     - command to generate model output
-# EVALUATE   - command to generate OUTPUTS from OLD_INPUTS
-# INFILE     - name of the input file for EVALUATE command, must exist
-# OUTFILE    - name of the output file for EVALUATE command
-# OUTPUTS    - list of OUTFILEs 
-# NEW_INPUTS - INFILEs for the next step, the goal of this make step
-export GENPARS RUNEPS
-export INFILE OUTFILE OUTPUTS NEW_INPUTS DATA cdate PPFILE OUTPUTS2
-
-# TARGET_GOAL   - the final target that make will try to reach
-# TARGET_SECND  - the target required by TARGET_GOAL
-# TARGET_FIRST  - the target required by TARGET_SECND
-# TARGET_ZERO   - the target required by TARGET_FIRST
+# TARGET_A0        - the final target that make will try to reach
+# TARGET_A1:N      - dependencies of TARGET_A
+# ALL_TARGETS_A1:N - dependencies as a complete list
 #
-# RULE_GOAL     - commands to execute once TARGET_SECND is available
-# RULE_SECND    - commands to execute once TARGET_FIRST is available
-# RULE_FIRST    - commands to execute once TARGET_START is available
-# RULE_ZERO     - commands to execute if nothing has yet been done
+# TARGET_B0        - what TARGET_B will produce
+# TARGET_B1:N      - dependencies of TARGET_B
+#
+# TARGET_C0        - what TARGET_C will produce
+# TARGET_C1:N      - dependencies of TARGET_C
+#
+# TARGET_D0        - what TARGET_D will produce
+# TARGET_D1:N      - dependencies of TARGET_D
+#
+# TARGET_E0        - what TARGET_E will produce
+# TARGET_E1:N      - dependencies of TARGET_E
+#
+# RULE_A    - commands to execute once TARGET_A1:N are available
+# RULE_B    - commands to execute once TARGET_B1:N are available
+# RULE_C    - commands to execute once TARGET_C1:N are available
+# RULE_D    - commands to execute once TARGET_D1:N are available
+# RULE_E    - commands to execute once TARGET_E1:N are available
 
-TARGET_GOAL=date_finished
-TARGET_SECND=ppfile
-TARGET_FIRST=outfile
-TARGET_ZERO=infile
+TARGET_E0=%/infile
+TARGET_E1=""
+RULE_E='cd $(dir $@) ;  cp -f infile_new infile'
+export TARGET_E0 TARGET_E1 RULE_E
 
-RULE_GOAL="echo > date_finished"
-RULE_SECND="cd $@ ; echo > ppfile"
-RULE_FIRST="cd $@ ; echo > outfile"
-RULE_ZERO="cd $@ ;  echo > infile"
+TARGET_D0=%/outfile
+TARGET_D1=%/infile
+RULE_D='cd $(dir $@) ; echo > outfile'
+export TARGET_D0 TARGET_D1 RULE_D
+
+TARGET_C0=\${ndate}/%/infile_new
+TARGET_C1=\${cdate}/%/outfile
+RULE_C='mkdir -p $(dir $@); cd $(dir $@); echo > infile_new'
+export TARGET_C0 TARGET_C1 RULE_C
+
+TARGET_B0=%/ppfile
+TARGET_B1=%/outfile
+RULE_B='cd $(dir $@) ; echo > ppfile'
+export TARGET_B0 TARGET_B1 RULE_B
+
+TARGET_A0=\${cdate}/date_finished
+TARGET_A1=ppfile
+TARGET_A2=infile_new
+RULE_A='echo > ${cdate}/date_finished'
+export TARGET_A0 ALL_TARGETS_A RULE_A
 
 # Set programs
-pargen=${SCRI}/pargen
 makefile=${SCRI}/makefile
-
-INFILE=input
-OUTFILE=output
-PPFILE=pp
-
-GENPARS="echo"
-RUNEPS="echo > output"
 
 
 cd $DATA
+export cdate ndate
 cdate=$SDATE
 while [ $cdate -le $EDATE ]; do
     # Log
     echo                                >> $WORK/master.log
     echo "Running ens for $cdate"       >> $WORK/master.log
     date | echo `exec cut -b13-21` init >> $WORK/master.log
-
     echo "   Processing date $cdate"
-    ndate=`exec $SCRI/./mandtg $cdate + $DSTEP`
-    flist=$(ls $cdate/pert*/input)
-    flist=${flist//[$'\t\r\n']/ }
-    OUTPUTS=${flist//\/input/\/output}
-    OUTPUTS2=${flist//\/input/\/pp}
-    NEW_INPUTS=${flist//${cdate}\//${ndate}\/}
     
-    # temp solution
-    mkdir -p $cdate/inistates
+    # Define next date
+    ndate=`exec $SCRI/./mandtg $cdate + $DSTEP`
+
+    # List all perturbation folders, trim and turn into an array
+    flist=$(ls -d $cdate/pert*)
+    flist=${flist//[$'\t\r\n']/ }; flist=( $flist )
+    flist_nextdate=${flist[@]//${cdate}\//${ndate}\/}; flist_nextdate=( $flist_nextdate )
+
+    # Create list of items for ALL_TARGETS_A
+    # (add TARGET_A1 after flist elements and TARGET_A2 after flist_nextdate elements)
+    ALL_TARGETS_A1=${flist[@]/%//${TARGET_A1}}
+    ALL_TARGETS_A2=${flist_nextdate[@]/%//${TARGET_A2}}
+    ALL_TARGETS_A="$ALL_TARGETS_A1 $ALL_TARGETS_A2"
 
     makemake > ${cdate}/foomakefile
     make -f $makefile -j $PARALLELS_IN_NODE
