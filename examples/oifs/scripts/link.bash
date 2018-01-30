f0f954dbedfd27bdace7f078b7f9e85696375b21
pirkkao <pirkka.ollinaho@fmi.fi>
2018-01-30 15:11:53 +0200

    1: enable running on taito-production nodes, 2: enable use of initial state perturbations, 3: added a few example exps for running on TL159

--- examples/oifs/scripts/link.bash
+++ examples/oifs/scripts/link.bash
@@ -19,9 +19,13 @@ fi
 # Copy namelist
 cp -f $SCRI/namelist.$name fort.4
 
-# All initial states link to control until the states can be generated
-name=ctrl
+# Under which alias the unperturbed ini is
+ename=oifs
 
+# Set initial perturbations off if INIPERT variable is not set
+if [ -z $INIPERT ]; then
+    INIPERT=0
+fi
 
 
 rm -f ICM*+00*
@@ -29,11 +33,17 @@ rm -f ICM*+00*
 # Link day specific fields
 if [ $RES -ne 21 ]; then # T21 does not need the climate file, also point all
                          # perts to ctrl
-    ln -sf ${IFSDATA}/t${RES}/$date/ctrl_ICMCL_INIT  ICMCL${EXPS}INIT
+    ln -sf ${IFSDATA}/t${RES}/$date/ICMCL${ename}INIT.1  ICMCL${EXPS}INIT
 fi
-ln -sf ${IFSDATA}/t${RES}/$date/${name}_ICMGG_INIT  ICMGG${EXPS}INIT
-ln -sf ${IFSDATA}/t${RES}/$date/${name}_ICMGG_INIUA ICMGG${EXPS}INIUA
-ln -sf ${IFSDATA}/t${RES}/$date/${name}_ICMSH_INIT  ICMSH${EXPS}INIT
+if [ $nid -eq 0 ] || [ $INIPERT -eq 0 ]; then
+    ln -sf ${IFSDATA}/t${RES}/$date/ICMGG${ename}INIT  ICMGG${EXPS}INIT
+    ln -sf ${IFSDATA}/t${RES}/$date/ggml$RES           ICMGG${EXPS}INIUA
+    ln -sf ${IFSDATA}/t${RES}/$date/ICMSH${ename}INIT  ICMSH${EXPS}INIT
+else
+    ln -sf ${IFSDATA}/t${RES}/$date/psu_$nid  ICMGG${EXPS}INIT
+    ln -sf ${IFSDATA}/t${RES}/$date/pan_$nid  ICMGG${EXPS}INIUA
+    ln -sf ${IFSDATA}/t${RES}/$date/pua_$nid  ICMSH${EXPS}INIT
+fi
 
 # Link climatologies
 ln -sf ${IFSDATA}/climatology/ifsdata .
